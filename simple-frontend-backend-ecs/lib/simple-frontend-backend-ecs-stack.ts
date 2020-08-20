f53bc2e68f098241829915069adb23ecdc994d1d
Chilcano <chilcano@intix.info>
2020-08-20 11:18:58 +0200

    CloudFormation and  its diagram were added

--- simple-frontend-backend-ecs/lib/simple-frontend-backend-ecs-stack.ts
+++ simple-frontend-backend-ecs/lib/simple-frontend-backend-ecs-stack.ts
@@ -9,7 +9,7 @@ export class SimpleFrontendBackendEcsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "SimpleFeBeVpc", { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, "FeBeVpc", { maxAzs: 2 });
     const cluster = new ecs.Cluster(this, "EcsCluster", { vpc: vpc });
     cluster.addDefaultCloudMapNamespace({ name: "service.local" });
 
