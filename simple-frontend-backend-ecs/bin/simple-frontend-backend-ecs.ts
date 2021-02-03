432c1f49d9360cdf8953322ed9168ee1e46105d6
chilcano <chilcano@intix.info>
2021-02-03 12:25:08 +0100

    ignored dirs removed

--- simple-frontend-backend-ecs/bin/simple-frontend-backend-ecs.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { SimpleFrontendBackendEcsStack } from '../lib/simple-frontend-backend-ecs-stack';
-
-const app = new cdk.App();
-new SimpleFrontendBackendEcsStack(app, 'SimpleFrontendBackendEcsStack');
--- test01/test/test01.test.ts
+++ test01/test/test01.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as SimpleFrontendBackendEcs from '../lib/simple-frontend-backend-ecs-stack';
+import * as Test01 from '../lib/test01-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new SimpleFrontendBackendEcs.SimpleFrontendBackendEcsStack(app, 'MyTestStack');
+    const stack = new Test01.Test01Stack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
