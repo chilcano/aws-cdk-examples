de1722edfc055ea8d3d5a3a2db036a376506fe97
chilcano <chilcano@intix.info>
2021-02-03 12:35:12 +0100

    ignored dirs removed

--- test01/bin/test01.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { Test01Stack } from '../lib/test01-stack';
-
-const app = new cdk.App();
-new Test01Stack(app, 'Test01Stack');
