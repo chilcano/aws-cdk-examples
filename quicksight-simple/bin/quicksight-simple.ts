87597f5c8ae5ce01ccd49eed82f510be57f9b35e
Chilcano <chilcano@intix.info>
2021-03-09 10:36:53 +0100

    removed no used dirs

--- quicksight-simple/bin/quicksight-simple.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { QuicksightSimpleStack } from '../lib/quicksight-simple-stack';
-
-const app = new cdk.App();
-new QuicksightSimpleStack(app, 'QuicksightSimpleStack');
