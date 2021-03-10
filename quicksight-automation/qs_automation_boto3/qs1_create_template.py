c092b0fda59be151bbdf4d49f1baa57a52ff8fff
Chilcano <chilcano@intix.info>
2021-03-10 19:39:47 +0100

    QuickSight resources added

--- quicksight-automation/qs_automation_boto3/qs1_create_template.py
+++ quicksight-automation/qs_automation_boto3/qs1_create_template.py
@@ -1,12 +1,13 @@
 import time
 import sys
+import boto
 import boto3
 from botocore.config import Config
 
 acc_id='601163517885'
-# ireland: eu-west-2, london: eu-west-1
+# ireland: eu-west-1, london: eu-west-2
 cfg = Config(
-    region_name = 'eu-west-2', # ireland
+    region_name = 'eu-west-2',
     signature_version = 'v4',
     retries = {
         'max_attempts': 10,
@@ -14,18 +15,11 @@ cfg = Config(
     }
 )
 
-client_qs = boto3.client('quicksight', config = cfg)
-
-response_qs = client_qs.
+qs_analysis_arn=""
+client_qs = boto3.client('quicksight')
 
 response_qs = client_qs.list_data_sets(
     AwsAccountId = acc_id,
     MaxResults = 100
 )
-i = 0
-for r in response_qs['DataSetSummaries']:
-    dataset_info = {}
-    dataset_info['id'] = r['DataSetId']
-    dataset_info['name'] = r['Name']
-    i = i+1
-    print ('\t', i, '\t-> DataSet name: ' + dataset_info['name'] + ' -> Id: ' + dataset_info['id'])
+
