d593c74c8bc246923ed75a191c3e77afd0daa9cf
Chilcano <chilcano@intix.info>
2021-03-11 10:30:59 +0100

    QuickSight moved out

--- quicksight-automation/lambda_hello/hello.py
+++
@@ -1,3 +0,0 @@
-def handler(event, context):
-	message = f'Hello {event["first_name"]} {event["last_name"]}!'
-	return {'message': message, 'event': event}
--- quicksight-automation/lambda_start_ec2_instance/start_ec2instance.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-_ec2_instance_id='i-07285b4b01414531c'
-
-ec2 = boto3.client('ec2')
-def start_ec2instance(event, context):
-    ec2.start_instances(InstanceIds= [_ec2_instance_id])
-    
