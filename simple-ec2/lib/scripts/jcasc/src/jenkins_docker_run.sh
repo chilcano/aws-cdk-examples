29699ef89ca3dd19a9b6e8e65e3d353168777382
Chilcano <chilcano@intix.info>
2021-08-25 22:15:37 +0200

    JCasC scripts updated

--- simple-ec2/lib/scripts/jcasc/src/jenkins_docker_run.sh
+++ simple-ec2/lib/scripts/jcasc/src/jenkins_docker_run.sh
@@ -14,4 +14,4 @@ keytore_pass=`cat ${JENKINS_KEYSTORE_PASS_FILE}`
 # Keep this directory for persistency
 volume_base=`dirname $(pwd)`/run/jenkins_home
 
-docker run --name jenkins --rm  -p 443:8443 -v $volume_base:/var/jenkins_home --env JENKINS_ADMIN_ID=$JENKINS_INITIAL_ADMIN_NAME --env JENKINS_ADMIN_PASSWORD="$psswrd" jenkins:jcasc --httpPort=-1 --httpsPort=8443 --httpsKeyStore=/usr/share/jenkins/ref/jenkins_keystore.jks --httpsKeyStorePassword="$keytore_pass"
+docker run --name jenkins --rm  -p 8443:8443 -v $volume_base:/var/jenkins_home --env JENKINS_ADMIN_ID=$JENKINS_INITIAL_ADMIN_NAME --env JENKINS_ADMIN_PASSWORD="$psswrd" jenkins:jcasc --httpPort=-1 --httpsPort=8443 --httpsKeyStore=/usr/share/jenkins/ref/jenkins_keystore.jks --httpsKeyStorePassword="$keytore_pass"
