b1d384f387670a14cff643745efc7690a3b0dc50
Chilcano <chilcano@intix.info>
2021-08-30 17:54:00 +0200

    Added images to simple-ec2

--- simple-ec2/_scripts/jcasc/src/jenkins_docker_run.sh
+++ simple-ec2/_scripts/jcasc/src/jenkins_docker_run.sh
@@ -2,12 +2,10 @@
 
 source jenkins_docker_set_envvars.sh
 
-echo -n "Starting Docker Jenkins container. Please enter password for '$JENKINS_INITIAL_ADMIN_NAME' user: "
-# read -es psswrd
-
 psswrd=`gpg --quiet --gen-random --armor 0 24 |& tail -1`
 
-echo $psswrd
+echo "Starting Docker Jenkins container."
+echo "Get access to Jenkins UI with '$JENKINS_INITIAL_ADMIN_NAME/$psswrd'."
 
 keytore_pass=`cat ${JENKINS_KEYSTORE_PASS_FILE}`
 
@@ -19,4 +17,4 @@ mkdir -p $volume_base
 chown -R 1000:1000 $volume_base
 
 # Running the container
-docker run --name jenkins --rm  -p 8443:8443 -v $volume_base:/var/jenkins_home --env JENKINS_ADMIN_ID=$JENKINS_INITIAL_ADMIN_NAME --env JENKINS_ADMIN_PASSWORD="$psswrd" jenkins:jcasc --httpPort=-1 --httpsPort=8443 --httpsKeyStore=/usr/share/jenkins/ref/jenkins_keystore.jks --httpsKeyStorePassword="$keytore_pass"
+docker run --name jenkins --detach --rm  -p 8443:8443 -v $volume_base:/var/jenkins_home --env JENKINS_ADMIN_ID=$JENKINS_INITIAL_ADMIN_NAME --env JENKINS_ADMIN_PASSWORD="$psswrd" jenkins:jcasc --httpPort=-1 --httpsPort=8443 --httpsKeyStore=/usr/share/jenkins/ref/jenkins_keystore.jks --httpsKeyStorePassword="$keytore_pass"
